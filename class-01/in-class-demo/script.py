1bdb7b793bcdd340f2f444a18ea4ef59e57fbaed
Adam Owada <adamowada@gmail.com>
2023-03-18 14:41:46 -0700

    fix: Corrected typo in print statement in script.py file.

--- class-01/in-class-demo/script.py
+++ class-01/in-class-demo/script.py
@@ -3,4 +3,4 @@
 # this block of code is only truthy if this file is executed as a script
 if __name__ == "__main__":
     print("Hello World!")
-    print("script,py's __name__ is:", __name__)
+    print("script.py's __name__ is:", __name__)
