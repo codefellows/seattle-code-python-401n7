7e3cac8c0934feb98e9c0bbed61cb498e674785c
Adam Owada <adamowada@gmail.com>
2023-03-18 14:40:43 -0700

    Add if statement to print 'Hello World!' only if executed as script

--- class-01/in-class-demo/script.py
+++ class-01/in-class-demo/script.py
@@ -1,1 +1,6 @@
-print("Hello World!")
+
+
+# this block of code is only truthy if this file is executed as a script
+if __name__ == "__main__":
+    print("Hello World!")
+    print("script,py's __name__ is:", __name__)
