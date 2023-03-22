5e661433a19aa8b7e92cecac4618fa350169f09b
Adam Owada <adamowada@gmail.com>
2023-03-21 20:10:14 -0700

    Add docstring to factorial function in recursion module.

--- class-02/in-class-demo/recursion_package/recursion_module.py
+++ class-02/in-class-demo/recursion_package/recursion_module.py
@@ -1,4 +1,9 @@
 def factorial(n):
+    """
+    Return n! representing the factorial
+    :param n: the number to get the factorial of
+    :return: n! aka n factorial
+    """
     # base case
     if n == 1:
         return 1
