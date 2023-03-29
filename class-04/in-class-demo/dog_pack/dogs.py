68007f9fcbabb0d8244b57f4f63ed25264255ea9
Adam Owada <adamowada@gmail.com>
2023-03-28 19:04:17 -0700

    "Added name attribute to Dog class constructor"

--- class-04/in-class-demo/dog_pack/dogs.py
+++ class-04/in-class-demo/dog_pack/dogs.py
@@ -3,12 +3,11 @@
 class Dog:
     # this gets invoked every time a new object is created
     # initialize the instance's attributes
-    def __init__(self, ):
-        pass
+    def __init__(self, name):
+        self.name = name
 
 
 if __name__ == "__main__":
     # create an instance of a Dog:
-    brock = Dog()
-
-
+    brock = Dog("Brock")
+    print(brock.name)
