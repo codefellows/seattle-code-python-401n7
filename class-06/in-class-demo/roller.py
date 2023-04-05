9b1ef709dc1171b583c9a6dbe3ceae2864763eb4
Adam Owada <adamowada@gmail.com>
2023-04-04 19:32:09 -0700

    Refactor dice_roller function to use loop instead of tuple comprehension.

--- class-06/in-class-demo/roller.py
+++ class-06/in-class-demo/roller.py
@@ -7,7 +7,13 @@ def dice_roller(n):
     :param n: Number of dice
     :return: Tuple of n integers, each integer being between 1 - 6 inclusive
     """
-    return tuple(random.randint(1, 6) for _ in range(n))
+    # One-liner with tuple comprehension
+    # return tuple(random.randint(1, 6) for _ in range(n))
+
+    dice = []
+    for i in range(n):
+        dice.append(random.randint(1, 6))
+    return tuple(dice)
 
 
 if __name__ == "__main__":
