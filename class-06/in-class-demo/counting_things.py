eb2bd6b400eb37ac88d1bfdc037596de82d9592f
Adam Owada <adamowada@gmail.com>
2023-04-04 20:00:55 -0700

    "Updated counting_things.py with roll_2 dictionary and roll_dict initialization."

--- class-06/in-class-demo/counting_things.py
+++ class-06/in-class-demo/counting_things.py
@@ -1,9 +1,12 @@
 # roll = (2, 1, 5, 4, 3, 4)
 
-roll = {4: 2, 5: 1, 3: 1, 2: 1, 1: 1}
-
-# roll_2 = (3, 3, 3, 5, 6, 5)
+# roll = {4: 2, 5: 1, 3: 1, 2: 1, 1: 1}
 
 # The keys are the sides of the dice, and the values are the occurances
-roll_2 = {3: 3, 5: 2, 6: 1}
+# roll_2 = {3: 3, 5: 2, 6: 1}
 
+roll_2 = (3, 3, 3, 5, 6, 5)
+
+roll_dict = {}
+for number in roll_2:
+    current_count = 
