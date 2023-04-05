1e4293d9c0677c6f18e654d3d567abca3d7c2ee2
Adam Owada <adamowada@gmail.com>
2023-04-04 19:20:03 -0700

    Add random module and implement dice rolling function in roller.py.

--- class-06/in-class-demo/roller.py
+++ class-06/in-class-demo/roller.py
@@ -1,11 +1,13 @@
+import random
+
 def dice_roller(n):
     """
     Rolls n dice.
     :param n: Number of dice
     :return: Tuple of n integers, each integer being between 1 - 6 inclusive
     """
-    pass
-
+    return tuple(random.randint(1, 6) for _ in range(n))
 
 if __name__ == "__main__":
-    dice_roller(6)
+    print(dice_roller(6))
+
