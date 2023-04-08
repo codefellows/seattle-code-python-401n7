17e99d29ced71e6534a625575f7ab2dc5c4e08ac
Adam Owada <adamowada@gmail.com>
2023-04-08 10:52:26 -0700

    Removed print statement from test_play_dice_quit function.

--- class-07/in-class-demo/tests/test_dice.py
+++ class-07/in-class-demo/tests/test_dice.py
@@ -19,7 +19,6 @@ def test_default_roller():
 # capsys will capture print
 def test_play_dice_quit(monkeypatch, capsys):
     monkeypatch.setattr("builtins.input", lambda x: "q")
-    print(input(""))
     play_dice(default_roller)
     captured = capsys.readouterr()
     # print("captured is:", captured)
