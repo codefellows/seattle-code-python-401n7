0d1d24470b37be44ab2b395a8d0cb270d851e094
Adam Owada <adamowada@gmail.com>
2023-04-08 09:48:34 -0700

    Add play_dice function and mock roller to dice.py script.

--- class-07/in-class-demo/dice.py
+++ class-07/in-class-demo/dice.py
@@ -1,6 +1,7 @@
 from random import randint
 
 
+# The actual random rolling function
 def default_roller():
     """
     Roll 2 six-sided dice
@@ -9,7 +10,27 @@ def default_roller():
     return tuple([randint(1, 6), randint(1, 6)])
 
 
-print(default_roller())
+def play_dice(roller):
+    """
+    Simulates a dice rolling game
+    :param roller: is a rolling function, that return a tuple of dice rolls
+    :return: None
+    """
+    while True:
+        print("Enter (r) to roll or (q) to quit")
+        choice = input("> ")
+
+        if choice == "q":
+            break
+
+        else:
+            roll = roller()
+            print(roll)
 
 
 
+# step? create a mock roller function!
+
+
+if __name__ == "__main__":
+    play_dice(default_roller)
