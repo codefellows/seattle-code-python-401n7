cfd52911a87643a493de5338140ddebbf300b208
Adam Owada <adamowada@gmail.com>
2023-04-08 09:20:14 -0700

    "Add new print function to overwrite old print"

--- class-07/in-class-demo/print_example.py
+++ class-07/in-class-demo/print_example.py
@@ -4,5 +4,16 @@ from builtins import print
 # save a reference to 'old print'
 old_print = print
 
-old_print("hello world")
+# old_print("hello world")
+
+
+# This function will overwrite print
+def new_print(name):
+    old_print("Hello " + name)
+
+
+print = new_print
+
+print("Adam")
+print("Hello world")
 
