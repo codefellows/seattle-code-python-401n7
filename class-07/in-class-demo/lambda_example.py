51a17ff49690392b2e1fc66d34597cc65c639b2d
Adam Owada <adamowada@gmail.com>
2023-04-08 10:14:48 -0700

    "Added lambda function examples to lambda_example.py"

--- class-07/in-class-demo/lambda_example.py
+++ class-07/in-class-demo/lambda_example.py
@@ -1,3 +1,12 @@
 add = lambda x, y: x + y
 
+
 print(add(5, 10))
+
+
+numbers = [1, 2, 3, 4, 5]
+
+# Using a lambda function to square each number in the list
+squares = list(map(lambda x: x ** 2, numbers))
+
+print(squares)  # Output: [1, 4, 9, 16, 25]
