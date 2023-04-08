ff1f831275999a8c8aa3baf0506e9b6a2f402cb9
Adam Owada <adamowada@gmail.com>
2023-04-08 10:26:58 -0700

    Add test for default_roller function in test_dice.py.

--- class-07/in-class-demo/tests/test_dice.py
+++ class-07/in-class-demo/tests/test_dice.py
@@ -5,3 +5,8 @@ from dice import default_roller, play_dice
 def test_exists():
     assert default_roller
     assert play_dice
+
+
+def test_default_roller():
+    roll = default_roller()
+    assert len(roll) == 2
