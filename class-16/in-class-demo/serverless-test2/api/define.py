e5bbab92c24e3dfbb0caa4b4ca4e6d369f9e1d8b
Adam Owada <adamowada@gmail.com>
2023-05-09 20:48:39 -0700

    corrected demo files

--- class-16/in-class-demo/serverless-test2/api/define.py
+++ class-16/in-class-demo/serverless-test2/api/define.py
@@ -1,32 +1,29 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-# new
 from urllib import parse
+# new
+import requests
 
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         # Take the url string, and create a dictionary of the parameters
         url = self.path  # this is the url string
-        print("self.path is:", url)
         url_components = parse.urlsplit(url)  # is the SplitResult() object
-        print("url_components is:", url_components)
         query_string_list = parse.parse_qsl(url_components.query)  # list of key value pair tuples, representing the query string
-        print("query_string_list is:", query_string_list)
         dictionary = dict(query_string_list)
-        print("dictionary is:", dictionary)
 
         # We can do stuff!
-        # Here we build up a message string
-        name = dictionary.get("name")
-        age = dictionary.get("age")
-        print("name is:", name)
-
-        if name and age:
-            message = f"Aloha {name}, you are {age} years old"
-        else:
-            message = "Aloha stranger!"
+        print(dictionary)
+        dictionary_api_url = f"https://api.dictionaryapi.dev/api/v2/entries/en/{dictionary.get('word')}"
+        print("dictionary_api_url is:", dictionary_api_url)
+        response = requests.get(dictionary_api_url)
+        data = response.json()
+        # print("data is:", data)
+        definition = data[0]["meanings"][0]["definitions"][0]["definition"]
+        print("definition is:", definition)
 
         # Forming the response
+        message = f"The meaning of {dictionary.get('word')} is: {definition}"
         self.send_response(200)  # HTTP code
         self.send_header('Content-type', 'text/plain')  # define the content type
         self.end_headers()  # add a blank line
