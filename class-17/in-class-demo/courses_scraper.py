38296abe01c6aa28158601edbb60d9dd6cfea2a0
Adam Owada <adamowada@gmail.com>
2023-05-13 09:57:19 -0700

    "Ignoring printed output, updated courses_scraper to print length of course_results."

--- class-17/in-class-demo/courses_scraper.py
+++ class-17/in-class-demo/courses_scraper.py
@@ -8,5 +8,5 @@ response = requests.get(url)
 soup = BeautifulSoup(response.content, "html.parser")
 
 course_results = soup.find_all(itemtype="http://schema.org/CourseInstance")
-print(course_results[0])
-
+# print(course_results[0])
+print(len(course_results))
