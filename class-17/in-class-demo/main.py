a6496886129696fa243862baad01a9904d98147b
Adam Owada <adamowada@gmail.com>
2023-05-13 09:31:39 -0700

    Added scraping instructions to main.py file.

--- class-17/in-class-demo/main.py
+++ class-17/in-class-demo/main.py
@@ -6,3 +6,7 @@ url = "https://adamowada.github.io/scrape-demo/"
 response = requests.get(url)
 
 print(response.content)
+
+# 1. Go to target website
+# 2. Find content you WANT to scrape
+# 3. Trial and error to target the content
