c6851a451b42423968074c89f562f1b91fe64a50
Adam Owada <adamowada@gmail.com>
2023-05-13 09:49:08 -0700

    Refactored courses_scaper.py: Updated URL and removed unnecessary h1_result variable.

--- class-17/in-class-demo/courses_scraper.py
+++ class-17/in-class-demo/courses_scraper.py
@@ -2,10 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 
 
-url = "https://adamowada.github.io/scrape-demo/"
+url = "https://testing-www.codefellows.org/course-calendar/?filters=400:%20Advanced,code-python-401"
 response = requests.get(url)
 
 soup = BeautifulSoup(response.content, "html.parser")
-h1_result = soup.find("h1")
 
-print(h1_result.text)
+print(soup)
