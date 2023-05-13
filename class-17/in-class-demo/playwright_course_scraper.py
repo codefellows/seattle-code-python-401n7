f82671672b595a1273caa14a8bfae3c7eb809b29
Adam Owada <adamowada@gmail.com>
2023-05-13 10:50:11 -0700

    "Update launch options for Playwright course scraper"

--- class-17/in-class-demo/playwright_course_scraper.py
+++ class-17/in-class-demo/playwright_course_scraper.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 def main():
     with sync_playwright() as playwright:
         # Open Chrome and navigate to my target page
-        browser = playwright.chromium.launch(headless=False, slow_mo=5000)
+        browser = playwright.chromium.launch(headless=False, slow_mo=2000)
         page = browser.new_page()
         page.goto("https://testing-www.codefellows.org/")  # this is where I put my URL
 
