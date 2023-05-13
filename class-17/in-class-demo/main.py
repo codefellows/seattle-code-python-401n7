1c9f4d5c56de15d01870a22af74f963b1cc50313
Adam Owada <adamowada@gmail.com>
2023-05-13 09:34:42 -0700

    "Added code to target and print h1 element's text"

--- class-17/in-class-demo/main.py
+++ class-17/in-class-demo/main.py
@@ -5,8 +5,15 @@ from bs4 import BeautifulSoup
 url = "https://adamowada.github.io/scrape-demo/"
 response = requests.get(url)
 
-print(response.content)
+# print(response.content)
 
 # 1. Go to target website
 # 2. Find content you WANT to scrape
 # 3. Trial and error to target the content
+
+# Target the h1 element's text
+
+soup = BeautifulSoup(response.content, "html.parser")
+
+print(type(soup))
+print(soup)
