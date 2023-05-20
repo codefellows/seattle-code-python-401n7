03c5c745062856f26e0e09237c76a1c2b373df19
Adam Owada <adamowada@gmail.com>
2023-05-20 10:24:16 -0700

    "Added functionality to create and write to a new file in a created directory using os module"

--- class-19/in-class-demo/os_demo.py
+++ class-19/in-class-demo/os_demo.py
@@ -15,3 +15,21 @@ print(os.listdir("."))
 shutil.rmtree('test/test_dir')
 print('test_dir deleted')
 
+
+# Join path components for the new file in the created directory
+file_path = os.path.join('test_dir', 'test_file.txt')
+print(f'Constructed file path: {file_path}')
+
+# Create a new file in the created directory
+with open(file_path, 'w') as file:
+    file.write('Hello, World!')
+
+# List files and directories in the created directory
+print('List of files/directories in test_dir:')
+print(os.listdir('test_dir'))
+
+# Split the file path into root and extension
+root, ext = os.path.splitext(file_path)
+print(f'Root of the file path: {root}')
+print(f'Extension of the file path: {ext}')
+
