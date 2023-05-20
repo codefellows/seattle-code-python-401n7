464a493c86993a8150f4ec9c992d934d90a4c58b
Adam Owada <adamowada@gmail.com>
2023-05-20 10:19:02 -0700

    Added directory creation and print statement to os_demo.py.

--- class-19/in-class-demo/os_demo.py
+++ class-19/in-class-demo/os_demo.py
@@ -1,6 +1,11 @@
 import os
 
 
+# Create a new directory
+os.mkdir('test_dir')
+print('Directory created.')
+
 # List files and directories in the current directory
 # List of files and directories
 print(os.listdir("."))
+
