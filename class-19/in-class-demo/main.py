18bfff23ec48dc459dc61c137df2ab9955343a05
Adam Owada <adamowada@gmail.com>
2023-05-20 09:45:29 -0700

    Added list_files function and choice handling in main entrypoint

--- class-19/in-class-demo/main.py
+++ class-19/in-class-demo/main.py
@@ -3,6 +3,10 @@ from rich.prompt import Prompt
 from rich.table import Table
 
 
+def list_files():
+    pass
+
+
 # Main entrypoint
 def main():
     """
@@ -12,6 +16,13 @@ def main():
     while True:
         console.print("\n1. List files\n2. Exit")
         choice = Prompt.ask("Choose a task or exit")
+
+        # if elif else block
+        if choice == "1":
+            # invoke list_files function
+            pass
+        elif choice == "2":
+            break
 
 
 if __name__ == "__main__":
