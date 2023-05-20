d1a5b527a3f69e47e4e854ca935a53f8ce35583b
Adam Owada <adamowada@gmail.com>
2023-05-20 09:35:19 -0700

    Added examples of text formatting and colorizing using the `rich` library.

--- class-19/in-class-demo/rich_demo.py
+++ class-19/in-class-demo/rich_demo.py
@@ -28,3 +28,25 @@ table.add_row(name, Prompt.ask("What is your age?"), Prompt.ask("What is your co
 
 # use rich print
 console.print(table)
+
+
+# More notes/examples
+
+# To change the color of the text, you can use square brackets to
+# enclose the name of the color and the text you want to colorize:
+console.print("[red]Hello, World![/red]")  # Prints "Hello, World!" in red
+# `rich` supports a wide variety of colors, including `red`, `green`, `blue`, `cyan`, `magenta`, `yellow`, and `black`.
+
+# You can make text bold, italic, underline, or strikethrough:
+console.print("[bold]Hello, World![/bold]")         # Prints "Hello, World!" in bold
+console.print("[italic]Hello, World![/italic]")     # Prints "Hello, World!" in italic
+console.print("[underline]Hello, World![/underline]") # Prints "Hello, World!" with an underline
+console.print("[strikethrough]Hello, World![/strikethrough]") # Prints "Hello, World!" with a strikethrough
+
+# Styles can be combined with colors:
+console.print("[bold red]Hello, World![/bold red]")  # Prints "Hello, World!" in bold red
+# You can nest styles within each other. The innermost style takes precedence:
+console.print("[red]Hello, [bold]World![/bold][/red]")  # Prints "Hello, " in red, and "World!" in bold red
+
+# Remember to close your tags with `[/]`. This tells `rich` where the style or color should end:
+console.print("[red]Hello, World![/red] This is not red.")  # Only "Hello, World!" is red
