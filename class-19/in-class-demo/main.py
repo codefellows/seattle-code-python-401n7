62f7ca9955af5ebbd5e6cf20f0763a759aeae09b
Adam Owada <adamowada@gmail.com>
2023-05-20 09:39:22 -0700

    "Refactored main function and removed print_hi function"

--- class-19/in-class-demo/main.py
+++ class-19/in-class-demo/main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from rich.console import Console
+from rich.prompt import Prompt
+from rich.table import Table
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Main entrypoint
+def main():
+    """
+    
+    :return:
+    """
+    pass
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+if __name__ == "__main__":
+    main()
