4aa1782538bbd174714e943fc46f0904f1d2d7ae
Adam Owada <adamowada@gmail.com>
2023-05-20 09:23:39 -0700

    Added user prompt for name and displayed personalized message.

--- class-19/in-class-demo/rich_demo.py
+++ class-19/in-class-demo/rich_demo.py
@@ -7,3 +7,9 @@ console = Console()
 
 # Greet the user
 console.print("Hello! [bold green]Hello again![/bold green]")
+
+# Ask for user's name
+name = Prompt.ask("What is your [bold]name?[/bold]")
+
+# Display a message to the user
+console.print(f"Nice to meet you, [bold blue]{name}[/bold blue]!")
