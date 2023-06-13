5f63a760a2f513b6f0e6d234cdc5142ba9aec5c2
Adam Owada <adamowada@gmail.com>
2023-06-12 19:47:55 -0700

    code review code

---
+++ class-26/code-review/django-things/things/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .views import AboutPageView, HomePageView
+
+
+urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
+    path('about', AboutPageView.as_view(), name='about'),
+]
