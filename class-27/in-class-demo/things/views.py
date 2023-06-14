53b2d7a761794b6f46aeed4babd1b0891556d08a
Adam Owada <adamowada@gmail.com>
2023-06-13 20:58:21 -0700

    finished demo code

--- class-27/in-class-demo/things/views.py
+++ class-27/in-class-demo/things/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Thing
 
-# Create your views here.
+
+class ThingListView(ListView):
+    template_name = "thing_list.html"
+    model = Thing
+    context_object_name = "things"
+
+
+class ThingDetailView(DetailView):
+    template_name = "thing_detail.html"
+    model = Thing
