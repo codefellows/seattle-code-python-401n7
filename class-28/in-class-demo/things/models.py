db751a8f8d4418f3362d03d06f728d4bcb310b11
Adam Owada <adamowada@gmail.com>
2023-06-17 10:03:06 -0700

    demo code 1

---
+++ class-28/in-class-demo/things/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+# 1. do models.py
+# 2. python manage.py makemigrations
+# 3. python manage.py migrate
+# 4. register Thing with admin.py
+
+
+class Thing(models.Model):
+    # Each class variable represents a field in the table
+    name = models.CharField(max_length=256)
+    rating = models.IntegerField(default=0)
+    reviewer = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
---
+++ class-28/in-class-demo/things/views.py
@@ -0,0 +1,16 @@
+from django.views.generic import ListView, DetailView
+from .models import Thing
+
+
+class ThingListView(ListView):
+    template_name = "thing_list.html"
+    model = Thing
+    context_object_name = "things"
+
+
+class ThingDetailView(DetailView):
+    template_name = "thing_detail.html"
+    model = Thing
+
+
+# More CRUD to come!
