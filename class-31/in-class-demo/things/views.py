012ebd80a83070f574e67c8dc48392d8d119187d
Adam Owada <adamowada@gmail.com>
2023-06-27 19:55:33 -0700

    Update template extends to use _base.html instead of base.html, delete README.md file, and add ThingList view to things views.

--- class-31/in-class-demo/things/views.py
+++ class-31/in-class-demo/things/views.py
@@ -1,1 +1,8 @@
 from rest_framework import generics
+from .serializers import ThingSerializer
+from .models import Thing
+
+
+class ThingList(generics.ListAPIView):
+    queryset = Thing.objects.all()
+    
