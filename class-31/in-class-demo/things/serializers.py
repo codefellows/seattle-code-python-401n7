5cd950fb36143a45a775031d92ae4f38bd429f5d
Adam Owada <adamowada@gmail.com>
2023-06-27 19:52:24 -0700

    Refactored templates to extend from _base.html instead of base.html and added ThingSerializer with specified fields.

--- class-31/in-class-demo/things/serializers.py
+++ class-31/in-class-demo/things/serializers.py
@@ -1,7 +1,13 @@
 # Serializers change the django models into JSON
 from rest_framework import serializers
+# The model we are serializing (representing as JSON)
 from .models import Thing
 
 # Take `Thing` and make it JSON. Like a translator
 
-class ThingSerializer(serializers.ModelSerializer)
+
+class ThingSerializer(serializers.ModelSerializer):
+    # Defines an inner class, Meta, which is used to specify metadata about the serializer class.
+    class Meta:
+        fields = ('id', 'owner', 'name', 'description', 'created_at')  # could also use '__all__'
+        model = Thing  # the model to serialize
