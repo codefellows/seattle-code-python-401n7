645c009c4d894accdba01ae48a61a4b748e0190e
Adam Owada <adamowada@gmail.com>
2023-06-27 20:12:17 -0700

    "Modified templates and views in djangox-demo, deleted class-31 README file, and updated ThingList and ThingDetail classes in views.py."

--- class-31/in-class-demo/things/views.py
+++ class-31/in-class-demo/things/views.py
@@ -3,12 +3,12 @@ from .serializers import ThingSerializer
 from .models import Thing
 
 
-class ThingList(generics.ListAPIView):
+class ThingList(generics.ListCreateAPIView):
     queryset = Thing.objects.all()
     serializer_class = ThingSerializer
 
 
-class ThingDetail(generics.RetrieveAPIView):
+class ThingDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Thing.objects.all()
     serializer_class = ThingSerializer
 
