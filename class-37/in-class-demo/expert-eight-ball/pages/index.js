e17a0ff55d4db93338fc678d17fb4218ff3c187f
Adam Owada <adamowada@gmail.com>
2023-07-18 19:31:21 -0700

    Refactor index.js file to improve code structure and readability.

--- class-37/in-class-demo/expert-eight-ball/pages/index.js
+++ class-37/in-class-demo/expert-eight-ball/pages/index.js
@@ -2,36 +2,37 @@
 import Head from "next/head";
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Expert Eight Ball</title>
-      </Head>
+    return (
+        <>
+            <Head>
+                <title>Expert Eight Ball</title>
+            </Head>
 
-      <header className="flex item-center justify-between bg-gray-500 text-gray-50 p-4">
-        <h1>Expert 8 Ball</h1>
-        <p>1 question answered</p>
-      </header>
+            <header className="flex item-center justify-between bg-gray-500 text-gray-50 p-4">
+                <h1>Expert 8 Ball</h1>
+                <p>1 question answered</p>
+            </header>
 
-      <main className="flex flex-col items-center py-4 space-y-8">
-        {/* Question Form */}
-        <form className="flex w-1/2 p-2 my-4 bg-gray-200">
-          <input className="flex-auto pl-1" />
-          <button className="bg-gray-500 text-gray-50 px-2 py-1">Ask!</button>
-        </form>
+            <main className="flex flex-col items-center py-4 space-y-8">
+                {/* Question Form */}
+                <form className="flex w-1/2 p-2 my-4 bg-gray-200">
+                    <input className="flex-auto pl-1" />
+                    <button className="bg-gray-500 text-gray-50 px-2 py-1">
+                        Ask!
+                    </button>
+                </form>
 
-        {/* Eight Ball */}
-        <div className="w-96 h-96 bg-gray-900 rounded-full">
-          <div className="relative  w-48 h-48 bg-gray-50">
+                {/* Eight Ball */}
+                <div className="w-96 h-96 mx-auto my-4 bg-gray-900 rounded-full">
+                    <div className="relative flex items-center justify-center w-48 h-48 rounded-full bg-gray-50 top-16 left-16">
+                        <p className="text-xl text-center">Ask me anything</p>
+                    </div>
+                </div>
+            </main>
 
-          </div>
-        </div>
-
-      </main>
-
-      <footer className="bg-gray-500 text-gray-50 p-4">
-        <p>Expert 8 ball &copy;2023</p>
-      </footer>
-    </>
-  );
+            <footer className="bg-gray-500 text-gray-50 p-4">
+                <p>Expert 8 ball &copy;2023</p>
+            </footer>
+        </>
+    );
 }
