1d55bc5068145e7dce9d92aaafdbabfe396d3b3e
Adam Owada <adamowada@gmail.com>
2023-07-18 18:53:58 -0700

    Refactored index.js file to remove unused imports and eslint-disable comment.

--- class-37/in-class-demo/expert-eight-ball/pages/index.js
+++ class-37/in-class-demo/expert-eight-ball/pages/index.js
@@ -1,8 +1,5 @@
 /* eslint-disable indent */
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
