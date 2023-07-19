471d3ef21a939fd51d742e340a5232f1042aafc3
Adam Owada <adamowada@gmail.com>
2023-07-18 20:17:29 -0700

    Added functionality to display the most recent answered question.

--- class-37/in-class-demo/expert-eight-ball/pages/index.js
+++ class-37/in-class-demo/expert-eight-ball/pages/index.js
@@ -22,6 +22,7 @@ export default function Home() {
       if (answeredQuestions.length === 0) {
         return 'Ask a question!';
       }
+      return answeredQuestions[-1];
     }
 
     return (
