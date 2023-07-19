a0cda1b2d3196b4187501acd120c52b25ccf66ab
Adam Owada <adamowada@gmail.com>
2023-07-18 19:14:42 -0700

    Add header and footer components to the index page.

--- class-37/in-class-demo/expert-eight-ball/pages/index.js
+++ class-37/in-class-demo/expert-eight-ball/pages/index.js
@@ -7,15 +7,18 @@ export default function Home() {
       <Head>
         <title>Expert Eight Ball</title>
       </Head>
+
       <header className="flex item-center justify-between bg-gray-500 text-gray-50 p-4">
         <h1>Expert 8 Ball</h1>
         <p>1 question answered</p>
       </header>
+
       <main className="flex flex-col items-center py-4 space-y-8">
         <p>Sample text. Eight ball goes here</p>
       </main>
-      <footer>
 
+      <footer>
+        
       </footer>
     </>
   );
