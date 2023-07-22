63a8554c800a8525b7bc1734f0729fd27f84c4c9
Adam Owada <adamowada@gmail.com>
2023-07-22 12:34:32 -0700

    finished demo code with three.js

--- class-38/in-class-demo/expert-eight-ball-2/components/EightBall.js
+++ class-38/in-class-demo/expert-eight-ball-2/components/EightBall.js
@@ -1,9 +1,40 @@
+import React, { useEffect, useRef } from "react";
+import * as THREE from "three";
+
 export default function EightBall({ getLatestReply }) {
-    return (
-        <div className="animate-spin-slow w-96 h-96 mx-auto my-4 bg-custom-salmon rounded-full">
-            <div className="relative flex items-center justify-center w-48 h-48 rounded-full bg-gray-50 top-16 left-16">
-                <p className="text-xl text-center">{getLatestReply()}</p>
-            </div>
-        </div>
-    );
+  const mount = useRef(null);
+
+  useEffect(() => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(600, mount.current.clientWidth / mount.current.clientHeight, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer();
+
+    renderer.setSize(mount.current.clientWidth, mount.current.clientHeight);
+    mount.current.appendChild(renderer.domElement);
+
+    const geometry = new THREE.SphereGeometry(4, 32, 32);
+    const material = new THREE.MeshBasicMaterial({ color: 0xf08080 });
+    const sphere = new THREE.Mesh(geometry, material);
+
+    scene.add(sphere);
+
+    camera.position.z = 5;
+
+    const animate = function () {
+      requestAnimationFrame(animate);
+      sphere.rotation.x += 0.01;
+      sphere.rotation.y += 0.01;
+      renderer.render(scene, camera);
+    };
+
+    animate();
+
+    return () => {
+      // cleaning up the mount element when the component is unmounted.
+      while (mount.current.firstChild)
+        mount.current.removeChild(mount.current.firstChild);
+    }
+  }, []);
+
+  return <div ref={mount} className="w-96 h-96" />;
 }
