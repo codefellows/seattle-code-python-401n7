db8d37aa38acfe865647f508b33e05e5389b70d5
Adam Owada <adamowada@gmail.com>
2023-07-25 20:59:19 -0700

    completed demo code

--- class-39/in-class-demo/cookie-stand-admin/pages/index.js
+++ class-39/in-class-demo/cookie-stand-admin/pages/index.js
@@ -6,7 +6,6 @@ export default function Home() {
     // const user = null; // this will be authentication code
     // const user = { username: 'somebody' }; // this will be authentication code
     const { user, login, logout } = useAuth(); // user variable and login() function come from the useAuth hook
-    const { resources, loading } = useResource();
 
     return (
         <div>
@@ -30,7 +29,8 @@ export default function Home() {
                         >
                             Logout
                         </button>
-                        <StandList stands={resources} loading={loading} />
+                        {/* <StandList stands={resources} loading={loading} /> */}
+                        <CookieStandAdmin />
                     </>
                 ) : (
                     <>
@@ -69,16 +69,121 @@ function LoginForm({ onLogin }) {
     );
 }
 
-function StandList({ stands, loading }) {
-    if (loading) {
-        return <p>Loading...</p>;
+// Don't need a <li> we need a table!
+
+// function StandList({ stands, loading }) {
+//     if (loading) {
+//         return <p>Loading...</p>;
+//     }
+
+//     return (
+//         <ul>
+//             {stands.map((stand) => (
+//                 <li key={stand.id}>{stand.location}</li>
+//             ))}
+//         </ul>
+//     );
+// }
+
+function CookieStandAdmin() {
+    const { resources, deleteResource } = useResource();
+
+    return (
+        <>
+            <CookieStandForm />
+            <CookieStandTable
+                stands={resources || []}
+                deleteStand={deleteResource}
+            />
+        </>
+    );
+}
+
+function CookieStandForm() {
+    const { user } = useAuth();
+    const { createResource } = useResource();
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        const info = {
+            location: event.target.location.value,
+            minimum_customers_per_hour: parseInt(event.target.minimum.value),
+            maximum_customers_per_hour: parseInt(event.target.maximum.value),
+            average_cookies_per_sale: parseFloat(event.target.average.value),
+            owner: user.id,
+        };
+        createResource(info);
     }
 
     return (
-        <ul>
-            {stands.map((stand) => (
-                <li key={stand.id}>{stand.location}</li>
+        <form onSubmit={handleSubmit}>
+            <fieldset>
+                <legend>Create Cookie Stand</legend>
+                <input placeholder="location" name="location" />
+                <input placeholder="minimum" name="minimum" />
+                <input placeholder="maximum" name="maximum" />
+                <input placeholder="average" name="average" />
+                <button>create</button>
+            </fieldset>
+        </form>
+    );
+}
+
+function CookieStandTable({ stands, deleteStand }) {
+    return (
+        <table className="my-8">
+            <thead>
+                <tr>
+                    <th>Location</th>
+                    <th>6 am</th>
+                    <th>7 am</th>
+                    <th>8 am</th>
+                    <th>9 am</th>
+                    <th>10 am</th>
+                    <th>11 am</th>
+                    <th>12 pm</th>
+                    <th>1 pm</th>
+                    <th>2 pm</th>
+                    <th>3 pm</th>
+                    <th>4 pm</th>
+                    <th>5 pm</th>
+                    <th>6 pm</th>
+                    <th>7 pm</th>
+                    <th>totals</th>
+                </tr>
+            </thead>
+            <tbody>
+                {stands.map((stand) => (
+                    <CookieStandRow
+                        key={stand.id}
+                        info={stand}
+                        deleteStand={deleteStand}
+                    />
+                ))}
+            </tbody>
+        </table>
+    );
+}
+
+function CookieStandRow({ info, deleteStand }) {
+    function clickHandler() {
+        deleteStand(info.id);
+    }
+
+    if (info.hourly_sales.length == 0) {
+        // bunk data
+        info.hourly_sales = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    }
+
+    return (
+        <tr>
+            <td>
+                {info.location} <button onClick={clickHandler}>[x]</button>
+            </td>
+            {info.hourly_sales.map((slot, index) => (
+                <td key={index}>{slot}</td>
             ))}
-        </ul>
+            <td>{info.hourly_sales.reduce((num, sum) => num + sum, 0)}</td>
+        </tr>
     );
 }
