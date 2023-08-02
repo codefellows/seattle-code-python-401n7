460fdcf4a3988afbe14be16a580eb827de0d5df2
Adam Owada <adamowada@gmail.com>
2023-08-01 18:26:37 -0700

    demo starter code from class 39

---
+++ class-41/in-class-demo/cookie-stand-admin/pages/_app.js
@@ -0,0 +1,10 @@
+import "@/styles/globals.css";
+import { AuthProvider } from "@/contexts/auth";
+
+export default function App({ Component, pageProps }) {
+    return (
+        <AuthProvider>
+            <Component {...pageProps} />
+        </AuthProvider>
+    );
+}
